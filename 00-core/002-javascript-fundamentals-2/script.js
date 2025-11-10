52e29fad2e2ca282a6746570af885c99e3fc885d
Tyler Jarvis <tyler.jarvis@westernpower.com.au>
2025-11-10 15:12:55 +0800

    learning content

--- 00-core/002-javascript-fundamentals-2/script.js
+++ 00-core/002-javascript-fundamentals-2/script.js
@@ -23,16 +23,27 @@
 //   console.log("Tyler is not a friend!");
 // }
 
-const bills = [125, 555, 44];
+// const bills = [125, 555, 44];
 
-const calcTip = function (bill) {
-  if (bill > 50 && bill < 300) {
-    return bill * 0.15;
-  } else {
-    return bill * 0.2;
-  }
+// const calcTip = function (bill) {
+//   if (bill > 50 && bill < 300) {
+//     return bill * 0.15;
+//   } else {
+//     return bill * 0.2;
+//   }
+// };
+
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+// console.log(tips);
+
+const aboutMe = {
+  firstName: "Tyler",
+  lastName: "Jarvis",
+  partner: "Kate",
+  pets: ["Arlo", "Indi"],
 };
 
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(aboutMe.pets[0]);
