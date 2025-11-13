0ddd68444d1f216222631ffd65285670937317dd
Tyler Jarvis <tylerxjarvis@outlook.com>
2025-11-13 14:32:37 +0800

    learning content

--- 01-miscellaneous/011-developer-skills/script.js
+++ 01-miscellaneous/011-developer-skills/script.js
@@ -1,2 +1,8 @@
-// placeholder
-console.log("hello");
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+let str = "";
+for (let i = 0; i < data1.length; i++) {
+  str = str + `${data1[i]}*C...`;
+}
+console.log(str);
